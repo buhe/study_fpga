428f6188d65f98074e3453468bc8d7ac0a2f89cf
buhe <bugu1986@126.com>
2022-01-27 18:04:43 +0800

    add ch src

---
+++ Makefile
@@ -0,0 +1,2 @@
+g_v:
+	sbt "test:runMain test.testMain --target-dir generated/and"
---
+++ src/test/scala/ANDtest.scala
@@ -0,0 +1,10 @@
+
+// ANDtest.scala
+package test
+ 
+import chisel3._
+ 
+object testMain extends App {
+  val verilogString = chisel3.emitVerilog(new AND)
+println(verilogString)
+}
